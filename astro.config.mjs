f1a1f7625f2f526c2ddab4e27210f60cf05d8831
ddiu8081 <ddiu8081@gmail.com>
2023-03-03 02:12:17 +0800

    chore: use `markdown-it`

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,30 @@ import vercel from '@astrojs/vercel/edge'
 import unocss from 'unocss/astro'
 import { presetUno } from 'unocss'
 import presetAttributify from '@unocss/preset-attributify'
+import presetTypography from '@unocss/preset-typography'
 import solidJs from '@astrojs/solid-js'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     unocss({
-      presets: [presetAttributify(), presetUno()]
+      presets: [
+        presetAttributify(),
+        presetUno(),
+        presetTypography({
+          cssExtend: {
+            'p': {
+              margin: '0.5rem 0',
+            },
+            'code': {
+              color: '#8b5cf6',
+            },
+            'pre': {
+              'background-color': '#1e293b',
+            }
+          },
+        }),
+      ]
     }),
     solidJs()
   ],
--- src/components/MessageItem.tsx
+++ src/components/MessageItem.tsx
@@ -1,8 +1,7 @@
 import type { Accessor } from 'solid-js'
 import type { ChatMessage } from '../types'
-import { micromark } from 'micromark'
-import { gfm, gfmHtml } from 'micromark-extension-gfm'
-import { math, mathHtml } from 'micromark-extension-math'
+import MarkdownIt from 'markdown-it'
+// import Shiki from 'markdown-it-shiki'
 
 interface Props {
   role: ChatMessage['role']
@@ -16,27 +15,19 @@ export default ({ role, message }: Props) => {
     assistant: 'bg-gradient-to-r from-yellow-200 via-green-200 to-green-300',
   }
   const htmlString = () => {
-    const microMarkOptions = {
-      extensions: [
-        gfm(),
-        math(),
-      ],
-      htmlExtensions: [
-        gfmHtml(),
-        mathHtml(),
-      ],
-    }
+    const md = MarkdownIt()
+
     if (typeof message === 'function') {
-      return micromark(message(), microMarkOptions)
+      return md.render(message())
     } else if (typeof message === 'string') {
-      return micromark(message, microMarkOptions)
+      return md.render(message)
     }
     return ''
   }
   return (
-    <div class="flex py-4 gap-3" class:op-75={ role === 'user' }>
-      <div class={ `shrink-0 w-7 h-7 rounded-full op-80 ${ roleClass[role] }` }></div>
-      <div class="message text-slate leading-loose break-words overflow-hidden" innerHTML={htmlString()} />
+    <div class="flex py-2 gap-3 -mx-4 px-4 rounded-lg transition-colors md:hover:bg-slate/3" class:op-75={ role === 'user' }>
+      <div class={ `shrink-0 w-7 h-7 mt-4 rounded-full op-80 ${ roleClass[role] }` }></div>
+      <div class="message prose text-slate break-words overflow-hidden" innerHTML={htmlString()} />
     </div>
   )
 }
