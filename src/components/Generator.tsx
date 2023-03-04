4f9ee75b260791a40d2f6295f3f055d2d63a6e71
ddiu8081 <ddiu8081@gmail.com>
2023-03-04 23:34:51 +0800

    feat: stop & regenerate latest answers

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -1,11 +1,11 @@
-import { createSignal, For, Show } from 'solid-js'
+import { createSignal, For, Show, Index } from 'solid-js'
 import MessageItem from './MessageItem'
 import IconClear from './icons/Clear'
 import type { ChatMessage } from '@/types'
 
 export default () => {
   let inputRef: HTMLTextAreaElement
-  const [messageList, setMessageList] = createSignal<ChatMessage[]>([])
+  const [messageList, setMessageList] = createSignal<ChatMessage[]>([{"role":"user","content":"你好！"},{"role":"assistant","content":"\n\n你好！我是AI助手，有什么可以帮助你的吗？"},{"role":"user","content":"给我讲一个故事"},{"role":"assistant","content":"好的，这是一个叫做《勇士和怪兽》的故事：\n\n在一个遥远的国度里，有一个勇士，他的名字叫做杰克。他是这个国度最勇敢的战士，因为他曾经打败了一只凶猛的巨龙。杰克有一个朋友叫做汤姆，他是一个年轻的牧羊人。\n\n有一天，这个国度遭到了一只可怕的怪兽的攻击，这只怪兽是一个巨大的狮子。它每天晚上都会来到村庄，抓走一些羊和小孩。人们都非常害怕，因为他们不知道该怎么办。\n\n杰克听到了这个消息，他知道他必须要做些什么来帮助这个国度。他决定去打败这只怪兽，但他也知道这不是一件容易的事情。\n\n汤姆知道杰克的计划，他决定和杰克一起去。他知道他不能让杰克一个人去面对这个怪兽。于是，杰克和汤姆一起出发了。\n\n他们走了很长时间，终于来到了怪兽的巢穴。他们看到了那只巨大的狮子，它看起来非常凶猛。\n\n杰克和汤姆决定一起攻击怪兽。他们使用了各种武器，但是怪兽非常强大，他们一直无法打败它。他们都感到非常沮丧，但是他们不想放弃。\n\n最终，他们想到了一个计划。他们决定将怪兽引诱到一个陷阱里，然后用巨石砸死它。这个计划非常危险，但他们决定还是要试一试。\n\n他们开始引诱怪兽，怪兽跟着他们走，最终掉进了陷阱里。杰克和汤姆立刻用巨石砸向怪兽，最终成功地打败了它。\n\n人们听到了这个消息，他们非常高兴。他们感谢杰克和汤姆，因为他们的勇气和智慧，拯救了整个国度。\n\n故事告诉我们勇气和智慧的重要性，只有勇敢地面对困难，才能获得胜利。"}])
   const [currentAssistantMessage, setCurrentAssistantMessage] = createSignal('')
   const [loading, setLoading] = createSignal(false)
   const [controller, setController] = createSignal<AbortController>(null)
@@ -15,7 +15,6 @@ export default () => {
     if (!inputValue) {
       return
     }
-    setLoading(true)
     // @ts-ignore
     if (window?.umami) umami.trackEvent('chat_generate')
     inputRef.value = ''
@@ -26,73 +25,70 @@ export default () => {
         content: inputValue,
       },
     ])
+    requestWithLatestMessage()
+  }
 
-    for (;;) {
-      try {
-        const controller = new AbortController()
-        setController(controller)
-        const response = await fetch('/api/generate', {
-          method: 'POST',
-          body: JSON.stringify({
-            messages: messageList(),
-          }),
-          signal: controller.signal,
-        })
-        if (!response.ok) {
-          throw new Error(response.statusText)
-        }
-        const data = response.body
-        if (!data) {
-          throw new Error('No data')
-        }
-        const reader = data.getReader()
-        const decoder = new TextDecoder('utf-8')
-        let done = false
-
-        while (!done) {
-          const { value, done: readerDone } = await reader.read()
-          if (value) {
-            let char = decoder.decode(value)
-            if (char === '\n' && currentAssistantMessage().endsWith('\n')) {
-              continue
-            }
-            if (char) {
-              setCurrentAssistantMessage(currentAssistantMessage() + char)
-            }
-            window.scrollTo({top: document.body.scrollHeight, behavior: 'smooth'})
-          }
-          done = readerDone
-        }
-      } catch (e) {
-        console.error(e)
-
-        // Retry when the request is aborted by user.
-        if (e.name === 'AbortError') {
-          setCurrentAssistantMessage('')
-          continue
-        }
-
-        // Keep the assistant message when there is an error.
-        // This enables the user to retry.
-        setLoading(false)
-        setController(null)
-        return
-      }
-
-      break
-    }
-
-    setMessageList([
-      ...messageList(),
-      {
-        role: 'assistant',
-        content: currentAssistantMessage(),
-      },
-    ])
+  const requestWithLatestMessage = async () => {
+    setLoading(true)
     setCurrentAssistantMessage('')
-    setLoading(false)
-    setController(null)
-    inputRef.focus()
+    try {
+      const controller = new AbortController()
+      setController(controller)
+      const response = await fetch('/api/generate', {
+        method: 'POST',
+        body: JSON.stringify({
+          messages: messageList(),
+        }),
+        signal: controller.signal,
+      })
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
+      const data = response.body
+      if (!data) {
+        throw new Error('No data')
+      }
+      const reader = data.getReader()
+      const decoder = new TextDecoder('utf-8')
+      let done = false
+
+      while (!done) {
+        const { value, done: readerDone } = await reader.read()
+        if (value) {
+          let char = decoder.decode(value)
+          if (char === '\n' && currentAssistantMessage().endsWith('\n')) {
+            continue
+          }
+          if (char) {
+            setCurrentAssistantMessage(currentAssistantMessage() + char)
+          }
+          window.scrollTo({top: document.body.scrollHeight, behavior: 'smooth'})
+        }
+        done = readerDone
+      }
+    } catch (e) {
+      console.error(e)
+      setLoading(false)
+      setController(null)
+      return
+    }
+    archiveCurrentMessage()
+  }
+
+  const archiveCurrentMessage = () => {
+    if (currentAssistantMessage()) {
+      setMessageList([
+        ...messageList(),
+        {
+          role: 'assistant',
+          content: currentAssistantMessage(),
+        },
+      ])
+      setCurrentAssistantMessage('')
+      setLoading(false)
+      setController(null)
+      inputRef.focus()
+    }
   }
 
   const clear = () => {
@@ -101,9 +97,21 @@ export default () => {
     setCurrentAssistantMessage('')
   }
 
-  const retry = () => {
+  const stopStreamFetch = () => {
     if (controller()) {
       controller().abort()
+      archiveCurrentMessage()
+    }
+  }
+
+  const retryLastFetch = () => {
+    if (messageList().length > 0) {
+      const lastMessage = messageList()[messageList().length - 1]
+      console.log(lastMessage)
+      if (lastMessage.role === 'assistant') {
+        setMessageList(messageList().slice(0, -1))
+        requestWithLatestMessage()
+      }
     }
   }
 
@@ -118,9 +126,31 @@ export default () => {
 
   return (
     <div my-6>
-      <For each={messageList()}>{(message) => <MessageItem role={message.role} message={message.content} />}</For>
-      {currentAssistantMessage() && <MessageItem role="assistant" message={currentAssistantMessage} onRetry={retry} />}
-      <Show when={!loading()} fallback={() => <div class="h-12 my-4 flex items-center justify-center bg-slate bg-op-15 text-slate rounded-sm">AI is thinking...</div>}>
+      <Index each={messageList()}>
+        {(message, index) => (
+          <MessageItem
+            role={message().role}
+            message={message().content}
+            showRetry={() => (message().role === 'assistant' && index === messageList().length - 1)}
+            onRetry={retryLastFetch}
+          />
+        )}
+      </Index>
+      {currentAssistantMessage() && (
+        <MessageItem
+          role="assistant"
+          message={currentAssistantMessage}
+        />
+      )}
+      <Show
+        when={!loading()}
+        fallback={() => (
+          <div class="h-12 my-4 flex gap-4 items-center justify-center bg-slate bg-op-15 text-slate rounded-sm">
+            <span>AI is thinking...</span>
+            <div class="px-2 py-0.5 border border-slate text-slate rounded-md text-sm op-70 cursor-pointer hover:bg-slate/10" onClick={stopStreamFetch}>Stop</div>
+          </div>
+        )}
+      >
         <div class="my-4 flex items-center gap-2">
           <textarea
             ref={inputRef!}
