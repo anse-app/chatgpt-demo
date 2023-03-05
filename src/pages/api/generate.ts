efc71722dcb586e175e218e4f56a2bfe754cadde
st6091741 <126646972+st6091741@users.noreply.github.com>
2023-03-06 01:49:49 +0800

    Update generate.ts
    Provide Reverse Proxy for OpenAI API.

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -6,6 +6,9 @@ import { fetch, ProxyAgent } from 'undici'
 
 const apiKey = import.meta.env.OPENAI_API_KEY
 const https_proxy = import.meta.env.HTTPS_PROXY
+var base_url = import.meta.env.OPENAI_API_BASE_URL /* Provide Reverse Proxy for OpenAI API. */
+if(!base_url) base_url = 'https://api.openai.com';
+base_url = base_url.trim().replace(/\/$/,'');
 
 export const post: APIRoute = async (context) => {
   const body = await context.request.json()
@@ -22,7 +25,7 @@ export const post: APIRoute = async (context) => {
   // #vercel-end
 
   // @ts-ignore
-  const response = await fetch('https://api.openai.com/v1/chat/completions', initOptions) as Response
+  const response = await fetch(base_url + '/v1/chat/completions', initOptions) as Response
 
   return new Response(parseOpenAIStream(response))
 }
