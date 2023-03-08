31469dd980f2d9717d8e0dc7aec86b43614bf601
yzh990918 <251205668@qq.com>
2023-03-08 13:39:50 +0800

    chore: update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,47 +1,23 @@
-import { defineConfig } from 'astro/config'
-import unocss from 'unocss/astro'
-import { presetUno } from 'unocss'
-import presetAttributify from '@unocss/preset-attributify'
-import presetTypography from '@unocss/preset-typography'
-import solidJs from '@astrojs/solid-js'
-import vercelDisableBlocks from './plugins/vercelDisableBlocks'
-
-import node from '@astrojs/node'
-import vercel from '@astrojs/vercel/edge'
-
-const envAdapter = () => {
-  if (process.env.OUTPUT == 'vercel') {
-    return vercel()
-  } else {
-    return node({
-      mode: 'standalone'
-    })
-  }
-}
+import { defineConfig } from 'astro/config';
+import unocss from 'unocss/astro';
+import { presetUno } from 'unocss';
+import presetAttributify from '@unocss/preset-attributify';
+import presetTypography from '@unocss/preset-typography';
+import solidJs from '@astrojs/solid-js';
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    unocss({
-      presets: [
-        presetAttributify(),
-        presetUno(),
-        presetTypography({
-          cssExtend: {
-            "ul,ol": {
-              "padding-left": "2em"
-            }
-          }
-        }),
-      ]
-    }),
-    solidJs()
-  ],
+  integrations: [unocss({
+    presets: [presetAttributify(), presetUno(), presetTypography({
+      cssExtend: {
+        "ul,ol": {
+          "padding-left": "2em"
+        }
+      }
+    })]
+  }), solidJs()],
   output: 'server',
-  adapter: envAdapter(),
-  vite: {
-    plugins: [
-      process.env.OUTPUT == 'vercel' && vercelDisableBlocks(),
-    ]
-  },
+  adapter: netlify(),
+  vite: {}
 });
