02e971bddb9f084454e81c7b0a328dd62d34c0bf
yzh990918 <251205668@qq.com>
2023-03-10 20:12:15 +0800

    chore: update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import vercelDisableBlocks from './plugins/vercelDisableBlocks'
 
 import node from '@astrojs/node'
 import vercel from '@astrojs/vercel/edge'
-import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify/edge-functions';
 
 const envAdapter = () => {
   if (process.env.OUTPUT == 'vercel') {
