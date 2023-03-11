91c07461c72ce284062d56f8bdac9b3a2e965743
Zoranner <zoran_young@hotmail.com>
2023-03-12 00:21:56 +0800

    fix: align message hovered item with input box

--- src/components/MessageItem.tsx
+++ src/components/MessageItem.tsx
@@ -65,7 +65,7 @@ export default ({ role, message, showRetry, onRetry }: Props) => {
   }
 
   return (
-    <div class="py-2 -mx-4 px-4 transition-colors md:hover:bg-slate/3">
+    <div class="py-2 px-4 transition-colors md:hover:bg-slate/3">
       <div class="flex gap-3 rounded-lg" class:op-75={role === 'user'}>
         <div class={`shrink-0 w-7 h-7 mt-4 rounded-full op-80 ${roleClass[role]}`}></div>
         <div class="message prose break-words overflow-hidden" innerHTML={htmlString()} />
