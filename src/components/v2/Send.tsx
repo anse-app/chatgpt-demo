3b680d926cea368e2af3879242fd63e368944a77
ddiu8081 <ddiu8081@gmail.com>
2023-03-16 14:26:39 +0800

    feat(ui): empty state for send component

---
+++ src/components/v2/Send.tsx
@@ -0,0 +1,21 @@
+import { Show } from 'solid-js'
+import { useStore } from '@nanostores/solid'
+import { inputPrompt } from '@/strores/ui'
+
+const EmptyState = () => (
+  <div class="h-14 px-6 border-t flex flex-row items-center gap-2 transition-colors cursor-pointer hover:bg-darker">
+    <div class="flex-1 op-30">Enter Something...</div>
+    <div class="i-carbon-send op-50 text-xl" />
+  </div>
+)
+
+export default () => {
+  const $inputPrompt = useStore(inputPrompt)
+  return (
+    <>
+      <Show when={!$inputPrompt()}>
+        <EmptyState />
+      </Show>
+    </>
+  )
+}
--- src/strores/ui.ts
+++ src/strores/ui.ts
@@ -1,10 +1,5 @@
-import { atom, action } from 'nanostores'
+import { atom } from 'nanostores'
 
 export const sidebar = atom(false)
-export const toggleSidebar = action(sidebar, 'toggleSidebar', store => {
-  store.set(!store.get())
-})
 
-// export function addUser(user: User) {
-//   users.set([...users.get(), user]);
-// }
+export const inputPrompt = atom('')
