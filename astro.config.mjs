c0e2a3516bc1f08c9e2975d861a87fdf2af33ea8
Miguel Gargallo <mgargallo16@gmail.com>
2023-03-20 20:42:33 +0100

    Improved astro.config.mjs
    Improved astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
 import { defineConfig } from 'astro/config'
 import unocss from 'unocss/astro'
 import solidJs from '@astrojs/solid-js'
-import vercelDisableBlocks from './plugins/vercelDisableBlocks'
 
 import node from '@astrojs/node'
 import vercel from '@astrojs/vercel/edge'
-import netlify from "@astrojs/netlify/edge-functions";
+import netlify from '@astrojs/netlify/edge-functions'
+import vercelDisableBlocks from './plugins/vercelDisableBlocks'
 
 const envAdapter = () => {
-  if (process.env.OUTPUT == 'vercel') {
+  if (process.env.OUTPUT === 'vercel') {
     return vercel()
-  } else if (process.env.OUTPUT == 'netlify') {
+  } else if (process.env.OUTPUT === 'netlify') {
     return netlify()
   } else {
     return node({
-      mode: 'standalone'
+      mode: 'standalone',
     })
   }
 }
@@ -23,14 +23,14 @@ const envAdapter = () => {
 export default defineConfig({
   integrations: [
     unocss(),
-    solidJs()
+    solidJs(),
   ],
   output: 'server',
   adapter: envAdapter(),
   vite: {
     plugins: [
-      process.env.OUTPUT == 'vercel' && vercelDisableBlocks(),
-      process.env.OUTPUT == 'netlify' && vercelDisableBlocks(),
-    ]
+      process.env.OUTPUT === 'vercel' && vercelDisableBlocks(),
+      process.env.OUTPUT === 'netlify' && vercelDisableBlocks(),
+    ],
   },
-});
+})
