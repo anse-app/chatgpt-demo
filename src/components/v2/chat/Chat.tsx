bbaa19f0cf84000691fc65c41d5167a2c7f2c5e5
ddiu8081 <ddiu8081@gmail.com>
2023-03-22 13:52:55 +0800

    refactor: move currentChat logic to `stores`

--- src/components/v2/chat/Chat.tsx
+++ src/components/v2/chat/Chat.tsx
@@ -1,19 +1,14 @@
 import { useStore } from '@nanostores/solid'
-import { chatList, currentChatId } from '@/stores/chat'
+import { currentChat } from '@/stores/chat'
 
 import SingleChat from '../chat/SingleChat'
 
 export default () => {
-  // const $currentChat = useStore(currentChat)
-  const $currentChatId = useStore(currentChatId)
-  const $chatList = useStore(chatList)
-  const currentChat = () => {
-    return $chatList().find(chat => chat.id === $currentChatId())
-  }
-  const chatType = () => currentChat()?.type
+  const $currentChat = useStore(currentChat)
+  const chatType = () => $currentChat()?.type
   return (
     <>
-      <pre>{JSON.stringify(currentChat(), null, 4)}</pre>
+      <pre>{JSON.stringify($currentChat(), null, 4)}</pre>
       { chatType() === 'single' && <SingleChat /> }
     </>
   )
