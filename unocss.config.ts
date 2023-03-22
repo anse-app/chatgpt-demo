bdd34e1481c0c70b9ef0aebb45df9be477954394
yzh990918 <251205668@qq.com>
2023-03-22 11:33:28 +0800

    chore: update README, fix #303 bug

--- unocss.config.ts
+++ unocss.config.ts
@@ -19,7 +19,8 @@ export default defineConfig({
     presetTypography({
       cssExtend: {
         'ul,ol': {
-          'list-style-position': 'inside',
+          'padding-left': '2.25em',
+          'position': 'relative',
         },
       },
     }),
