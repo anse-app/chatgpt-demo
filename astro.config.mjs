0998579a5c7a6f1168515662717e4cf41973e06c
yzh990918 <251205668@qq.com>
2023-03-24 11:15:41 +0800

    feat: add PWA support, fix #139

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import unocss from 'unocss/astro'
 import solidJs from '@astrojs/solid-js'
 
 import node from '@astrojs/node'
+import { VitePWA } from 'vite-plugin-pwa'
 import vercel from '@astrojs/vercel/edge'
 import netlify from '@astrojs/netlify/edge-functions'
 import vercelDisableBlocks from './plugins/vercelDisableBlocks'
@@ -31,6 +32,46 @@ export default defineConfig({
     plugins: [
       process.env.OUTPUT === 'vercel' && vercelDisableBlocks(),
       process.env.OUTPUT === 'netlify' && vercelDisableBlocks(),
+      process.env.OUTPUT !== 'netlify' && VitePWA({
+        includeAssets: ['favicon.svg'],
+        registerType: 'autoUpdate',
+        manifest: {
+          name: 'ChatGPT-API Demo',
+          short_name: 'ChatGPT-API Demo',
+          description: 'A demo repo based on OpenAI GPT-3.5 Turbo API',
+          theme_color: '#212129',
+          background_color: '#ffffff',
+          icons: [
+            {
+              src: 'favicon-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'favicon-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: 'favicon-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
+          ],
+        },
+        workbox: {
+          globDirectory: 'dist',
+          navigateFallback: '/',
+        },
+        client: {
+          installPrompt: true,
+          periodicSyncForUpdates: 20,
+        },
+        devOptions: {
+          enabled: true,
+        },
+      }),
     ],
   },
 })
