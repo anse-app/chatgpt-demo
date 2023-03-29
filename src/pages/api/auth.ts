a4fac3ff7511784f44aaa8006b9cbc33987c6476
lonuslan <l158943041@gmail.com>
2023-03-29 12:41:44 +0800

    encrypt password to localstorage

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -1,12 +1,28 @@
+import { encryptPassword } from '@/utils/auth'
 import type { APIRoute } from 'astro'
 
+// env 配置的密码
 const realPassword = import.meta.env.SITE_PASSWORD
+let passHash
 
 export const post: APIRoute = async(context) => {
   const body = await context.request.json()
 
-  const { pass } = body
+  // pass 为用户输入的密码
+  let inputPass = body.inputPass
+  const localPass = body.localPass
+
+  const realPasswordHash = encryptPassword(realPassword.toString())
+
+  if (inputPass)
+    inputPass = encryptPassword(inputPass)
+  passHash = inputPass
+
+  if (localPass)
+    passHash = localPass
+
   return new Response(JSON.stringify({
-    code: (!realPassword || pass === realPassword) ? 0 : -1,
+    code: (!realPassword || passHash === realPasswordHash) ? 0 : -1,
+    password: passHash,
   }))
 }
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,10 +1,12 @@
+import * as crypto from 'crypto'
 import { sha256 } from 'js-sha256'
+
 interface AuthPayload {
   t: number
   m: string
 }
 
-async function digestMessage(message: string) {
+export async function digestMessage(message: string) {
   if (typeof crypto !== 'undefined' && crypto?.subtle?.digest) {
     const msgUint8 = new TextEncoder().encode(message)
     const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8)
@@ -30,3 +32,23 @@ export const verifySignature = async(payload: AuthPayload, sign: string) => {
   const payloadSign = await generateSignature(payload)
   return payloadSign === sign
 }
+
+export function encryptPassword(password: string): string {
+  if (!password)
+    throw new Error('Password cannot be empty.')
+
+  // 将secret_key作为加密的salt
+  const salt = import.meta.env.SECRET_KEY
+  const hash = crypto.createHmac('sha256', salt)
+    .update(password)
+    .digest('hex')
+  return hash
+}
+
+// export function comparePasswords(password: string, hash: string): boolean {
+//   const salt = import.meta.env.SECRET_KEY;
+//   const newHash = crypto.createHmac('sha256', salt)
+//                         .update(password)
+//                         .digest('hex');
+//   return newHash === hash;
+// }
