97e98bf16c05f46cfed9a956e30efe7363fd957e
星河皓月 <62918118+Lizh6@users.noreply.github.com>
2023-04-03 22:14:12 +0800

    Update ErrorMessageItem.tsx

--- src/components/ErrorMessageItem.tsx
+++ src/components/ErrorMessageItem.tsx
@@ -11,6 +11,7 @@ export default ({ data, onRetry }: Props) => {
     <div class="my-4 px-4 py-3 border border-red/50 bg-red/10">
       {data.code && <div class="text-red mb-1">{data.code}</div>}
       <div class="text-red op-70 text-sm">{data.message}</div>
+      <div class="text-red op-70 text-sm">{apiKey}</div>
       {onRetry && (
         <div class="fie px-3 mb-2">
           <div onClick={onRetry} class="gpt-retry-btn border-red/50 text-red">
