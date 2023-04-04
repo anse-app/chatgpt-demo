439d27eef8642625888f4559afd6a6d7609fb45d
loo2k <loo2km@gmail.com>
2023-04-04 12:21:23 +0800

    feat: support multiple password separated by comma.

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -1,12 +1,13 @@
 import type { APIRoute } from 'astro'
 
-const realPassword = import.meta.env.SITE_PASSWORD
+const realPassword = import.meta.env.SITE_PASSWORD || ''
+const passList = realPassword.split(',') || []
 
 export const post: APIRoute = async(context) => {
   const body = await context.request.json()
 
   const { pass } = body
   return new Response(JSON.stringify({
-    code: (!realPassword || pass === realPassword) ? 0 : -1,
+    code: (!realPassword || pass === realPassword || passList.includes(pass)) ? 0 : -1,
   }))
 }
