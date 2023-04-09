90edaf821d1895d1703b75ad95b8fe8efd38e901
ddiu8081 <ddiu8081@gmail.com>
2023-04-10 01:00:52 +0800

    feat(ui): use zag to build drawers; add transition

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,15 +1,23 @@
-import { useStore } from '@nanostores/solid'
+import * as dialog from '@zag-js/dialog'
+import { normalizeProps, useMachine } from '@zag-js/solid'
+import { Show, createMemo, createUniqueId } from 'solid-js'
+import { Transition } from 'solid-transition-group'
+import { Portal } from 'solid-js/web'
 import type { JSXElement } from 'solid-js'
 import type { WritableAtom } from 'nanostores'
 
 interface Props {
-  bindValue: WritableAtom
+  bindValue: WritableAtom<boolean>
   direction: 'top' | 'bottom' | 'left' | 'right'
   children: JSXElement
 }
 
 export default (props: Props) => {
-  const modelValue = useStore(props.bindValue)
+  const [state, send] = useMachine(dialog.machine({
+    id: createUniqueId(),
+    initialFocusEl: null,
+  }))
+  const api = createMemo(() => dialog.connect(state, send, normalizeProps))
 
   const containerBaseClass = {
     top: 'top-0 left-0 right-0 border-b',
@@ -18,23 +26,30 @@ export default (props: Props) => {
     right: 'top-0 right-0 bottom-0 border-l',
   }[props.direction]
 
-  const containerTransformClass = () => ({
-    top: modelValue() ? 'translate-y-0' : '-translate-y-full',
-    bottom: modelValue() ? 'translate-y-0' : 'translate-y-full',
-    left: modelValue() ? 'translate-x-0' : '-translate-x-full',
-    right: modelValue() ? 'translate-x-0' : 'translate-x-full',
-  }[props.direction])
+  props.bindValue.subscribe((show) => {
+    if (show)
+      api().open()
+    else
+      api().close()
+  })
 
   return (
-    <div class="fixed inset-0" classList={{ 'pointer-events-none': !modelValue() }}>
-      <div
-        class="bg-base absolute inset-0 transition-opacity ease-out"
-        classList={{ 'opacity-50': modelValue(), 'opacity-0': !modelValue() }}
-        onClick={() => props.bindValue.set(false)}
-      />
-      <div class={`bg-base absolute transition-transform ease-out max-w-screen max-h-screen overflow-auto border-base ${containerBaseClass} ${containerTransformClass()}`}>
-        { props.children }
-      </div>
-    </div>
+    <Transition name={`slide-${props.direction}`}>
+      <Show when={api().isOpen}>
+        <div class="fixed inset-0 z-20">
+          <Portal>
+            <div class="fixed inset-0 bg-base opacity-50" {...api().backdropProps} />
+          </Portal>
+          <div {...api().containerProps}>
+            <div {...api().contentProps} class={`bg-base absolute transition-transform ease-out max-w-screen max-h-screen overflow-auto border-base ${containerBaseClass}`}>
+              <button class="absolute top-4 right-4" {...api().closeTriggerProps}>
+                <div i-carbon-close class="text-xl" />
+              </button>
+              { props.children }
+            </div>
+          </div>
+        </div>
+      </Show>
+    </Transition>
   )
 }
