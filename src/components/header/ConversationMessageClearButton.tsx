1c4c781128af940248406cfc71bcbb00f9332afe
ddiu8081 <ddiu8081@gmail.com>
2023-04-13 22:56:55 +0800

    fix(ui): hide scroll banner when clear message

--- src/components/header/ConversationMessageClearButton.tsx
+++ src/components/header/ConversationMessageClearButton.tsx
@@ -1,16 +1,22 @@
 import { useStore } from '@nanostores/solid'
 import { currentConversationId } from '@/stores/conversation'
+import { scrollController } from '@/stores/ui'
 import { clearMessagesByConversationId } from '@/stores/messages'
 
 export default () => {
   const $currentConversationId = useStore(currentConversationId)
+
+  const handleClearMessage = () => {
+    clearMessagesByConversationId($currentConversationId())
+    scrollController().scrollToBottom()
+  }
 
   return (
     <>
       { $currentConversationId() && (
         <div
           class="fcc p-2 rounded-md text-xl hv-foreground"
-          onClick={() => clearMessagesByConversationId($currentConversationId())}
+          onClick={handleClearMessage}
         >
           <div i-carbon-clean />
         </div>
