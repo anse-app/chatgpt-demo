3c003b2eaa2292b58ee219e2556edb631f5ce8f9
lwh <471071259@qq.com>
2023-04-14 18:23:37 +0800

    新增接口

---
+++ src/pages/api/chat.ts
@@ -0,0 +1,40 @@
+// #vercel-disable-blocks
+import { ProxyAgent, fetch } from 'undici'
+// #vercel-end
+import { generatePayload, parseOpenAIStream } from '@/utils/openAI'
+import { verifySignature } from '@/utils/auth'
+
+import type { APIRoute } from 'astro'
+
+const apiKey = '1'
+
+const baseUrl = 'https://api.openai.com'
+
+
+export const post: APIRoute = async(context) => {
+  const body = await context.request.json()
+  const { apiKey, messages } = body
+  if (!messages) {
+    return new Response(JSON.stringify({
+      error: {
+        message: 'No input text.',
+      },
+    }), { status: 400 })
+  }
+  
+  const initOptions = generatePayload(apiKey, messages)
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  const response = await fetch(`${baseUrl}/v1/chat/completions`, initOptions).catch((err: Error) => {
+    console.error(err)
+    return new Response(JSON.stringify({
+      error: {
+        code: err.name,
+        message: err.message,
+      },
+    }), { status: 500 })
+  }) as Response
+
+  return parseOpenAIStream(response) as Response
+}
