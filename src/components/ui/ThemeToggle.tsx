355bf470223c4f306d911e533c4c3c8013cd755e
ddiu8081 <ddiu8081@gmail.com>
2023-04-19 04:18:54 +0800

    fix(ui): change site-theme meta tag

--- src/components/ui/ThemeToggle.tsx
+++ src/components/ui/ThemeToggle.tsx
@@ -1,16 +1,28 @@
 import { useDark } from 'solidjs-use'
-import { Show } from 'solid-js'
+import { Show, onMount } from 'solid-js'
 
 export default () => {
+  // TODO: it created twice, optimize that
   const [isDark, setIsDark] = useDark({
     storageKey: 'theme',
     valueDark: 'dark',
   })
 
+  onMount(() => {
+    document.querySelector('meta[name="theme-color"]')?.setAttribute('content', isDark() ? '#222222' : '#fafafa')
+  })
+
+  const handleDarkChanged = () => {
+    console.log('handleDarkChanged')
+    const dark = !isDark()
+    document.querySelector('meta[name="theme-color"]')?.setAttribute('content', dark ? '#222222' : '#fafafa')
+    setIsDark(dark)
+  }
+
   return (
     <div
       class="fi p-2 rounded-md cursor-pointer text-lg hv-base hv-foreground"
-      onClick={() => setIsDark(!isDark())}
+      onClick={handleDarkChanged}
     >
       <Show when={isDark()} >
         <div i-carbon-moon />
