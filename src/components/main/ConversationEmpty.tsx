a1b94d80bfd26ff28b7dbb3aaa4d278384e95320
ddiu8081 <ddiu8081@gmail.com>
2023-04-20 03:13:39 +0800

    fix(ui): adjust welcome page's width

--- src/components/main/ConversationEmpty.tsx
+++ src/components/main/ConversationEmpty.tsx
@@ -8,7 +8,7 @@ export default () => {
 
   return (
     <div class="fcc h-full">
-      <div class="flex flex-col gap-4 w-full max-w-md mx-18">
+      <div class="flex flex-col gap-4 w-full max-w-md mx-12 sm:mx-18">
         <div class="px-6 py-4 bg-base-100 border border-base rounded-lg">
           <h2 class="text-xs op-30 uppercase my-2">Recents</h2>
           <div class="flex flex-col items-start">
